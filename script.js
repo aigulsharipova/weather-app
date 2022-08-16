8a5154abca6de91721461b1b3a87bd316bc40d96
Aigul <sharipova.aig@gmail.com>
2022-08-16 08:47:53 -0400

    js link

---
+++ script.js
@@ -0,0 +1,23 @@
+const api = {
+  endpoint: "https://api.openweathermap.org/data/2.5/",
+  key: "62ec8868273035b80fb16c0e9f6eddeb" }
+
+const input = document.querySelector('#input');
+input.addEventListener('keydown', enter);
+
+function enter(e) {
+  if(e.keycode === 13) {
+    getInfo(input.value);
+  }
+}
+
+async function getInfo(dat) {
+  const res = await fetch(`${api.endpoint}weather?q=${data}&units=metric&appID=${api.key}`)
+  const result = await res.json();
+  displayResult(result);
+}
+
+function displayResult(result) {
+let city = document.querySelector('#city');
+city.textContent = `${result.name}`;
+}
