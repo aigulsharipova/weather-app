9dee1703de57effa6d27b7d6ce5849b4a28cc0be
Aigul <sharipova.aig@gmail.com>
2022-08-15 20:19:23 -0400

    function

--- sctipt.js
+++ sctipt.js
@@ -1,6 +1,12 @@
 const api = {
   endpoint: "https://api.openweathermap.org/data/2.5/",
-  key: "62ec8868273035b80fb16c0e9f6eddeb"
-}
+  key: "62ec8868273035b80fb16c0e9f6eddeb" }
+
 const input = document.querySelector('#input');
 input.addEventListener('keydown', enter);
+
+function enter(e) {
+  if(e.keycode === 13) {
+    getInfo()
+  }
+}
